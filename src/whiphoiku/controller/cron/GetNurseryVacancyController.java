3cab8eeea52c98fba4535aeb5a01ba681b3dffc8
mmike <takumi.tanaka246@gmail.com>
2014-07-26 19:23:24 +0900

    add to cron setting / Get Nursery Vacancy - logic empty : now datastore testing

---
+++ src/whiphoiku/controller/cron/GetNurseryVacancyController.java
@@ -0,0 +1,12 @@
+package whiphoiku.controller.cron;
+
+import org.slim3.controller.Controller;
+import org.slim3.controller.Navigation;
+
+public class GetNurseryVacancyController extends Controller {
+
+    @Override
+    public Navigation run() throws Exception {
+        return null;
+    }
+}
